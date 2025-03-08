24bb0abd065e0389b04206d7a592496a8bae69d7
Darsh20009 <youssefdarwish20009@gmail.com>
2025-03-08 09:29:35 +0200

    Added video upload & download functionality

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def home () :
- return "Hello, World!"
-if __name__ == "__main__":
- app.run(host="0.0.0.0" , port=5000)
+from flask import Flask, request,jsonify, send_file
+import os
+app = Flask(_name_)
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER,exist_ok=True)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+@app.route("/upload",methods=["POST"])
+def upload_video():
+ if "file"not in request.files:
+  return jsonify({"error": "No file part"}), 400
+file = request.files["file"]
+if file.filename == "":
+ return jsonify({"error": "No selected file "}), 400
+file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+file.save(file_path)
+return jsonify({"message": "File uploaded successfully" , "file_path":file_path}), 200
+@app.route("/download/<filename>",methods=["GET"])
+def download_video(filename):
+ file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+if no os.path.exists(file_path):
+ return jsonify({"error": "File not Found"}) , 404
+return send_file(file_path, as_attachment=True)
+if_name_ == "_main_":
+app.run(host="0.0.0.0" , port=5000)
